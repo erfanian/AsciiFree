f82b67d9634c1916abf96fa444df2dc00c5cdcb5
Eric Erfanian <eric.erfanian@gmail.com>
2013-04-09 22:29:23 -0500

    rudimentary user input check

--- __main__.py
+++ __main__.py
@@ -1,8 +1,14 @@
 #! /usr/bin/env python3.0
 
 import os
+import sys
 
 while True:
-	os.system('clear')
-	print('|')
-
+  os.system('clear')
+  print('|\n')
+  user_option = input("Enter 'Q' to quit.")
+  try:
+    if user_option.lower() == 'q': # makes everything lowercase so it is easier to handle
+      sys.exit("Game Terminated.")
+  except TypeError:
+    print('What?')
