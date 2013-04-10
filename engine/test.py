5198c821499e763d8eb00558d3cbe002cd9a62bb
ccorn90 <ccorn90@gmail.com>
2013-04-09 22:50:27 -0700

    Adding the basic folder structure for the system.  No classes yet, and an empty file in archi/ until we finalize the first spec for the engine components.

---
+++ engine/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3.0
+
+# engine/test.py
+# A generic testing script for the engine.  Modify as much as you want... it's just for testing.
+
+import sys
+
+print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!\nTESTING SCRIPT BEGIN\n!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
