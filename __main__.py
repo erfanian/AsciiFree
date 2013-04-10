19f56438aff32b3192014bedef3adf99de220441
Eric Erfanian <eric.erfanian@gmail.com>
2013-04-09 22:53:42 -0500

    Revert "switch to python2 syntax in prep for pygame"
    This reverts commit 4a8f5e676e98a737350740d61f9f302b23cba478.

--- __main__.py
+++ __main__.py
@@ -1,14 +1,12 @@
-#! /usr/bin/env python2.0
+#! /usr/bin/env python3.0
 
 import os
 import sys
-#http://www.pygame.org/wiki/about
-import pygame
 
 while True:
   os.system('clear')
   print('|\n')
-  user_option = raw_input("Enter 'Q' to quit.")
+  user_option = input("Enter 'Q' to quit.")
   try:
     if user_option.lower() == 'q': # makes everything lowercase so it is easier to handle
       sys.exit("Game Terminated.")
