7de5ffc923eb828d87969604b10ab7f4fa10308c
Eric Erfanian <eric.erfanian@gmail.com>
2013-04-10 19:44:23 -0500

    we have stable input

--- engine/inputEngine.py
+++ engine/inputEngine.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3.0
 
-#That was easy!
+#That was easy! http://docs.python.org/3.1/howto/curses.html
 import curses
 
 #Draw a screen
@@ -17,11 +17,13 @@ stdscr.keypad(1)
 
 while True:
 	#Don't wait for user input
-	stdscr.nodelay(1)
+	#stdscr.nodelay(1)
 	#Get the input
 	c = stdscr.getch()
 	#Get a visual on things
-	print('Hello')
+	stdscr.clear()	
+	stdscr.refresh()	
+	print(c)
 	if c == ord('q'):
 		#Give the user her session back
 		curses.endwin()
