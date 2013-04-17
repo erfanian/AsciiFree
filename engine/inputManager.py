53a3cadaabacbf00dfbe25fc9a0746f14d6dcefe
ccorn90 <ccorn90@gmail.com>
2013-04-16 19:01:44 -0700

    Making inputManager a Thread subclass to allow the run loops to work properly.  Modifying unittests.py to match.  Abstracting input char handling to handleInput()

--- engine/inputManager.py
+++ engine/inputManager.py
@@ -19,6 +19,7 @@ import curses
 import queue
 #http://docs.python.org/3.1/library/threading.html
 import threading
+import time
 
 class Screen:
 	'A generic screen class for now'
@@ -48,43 +49,91 @@ class Screen:
 		
 #------------- Above for things that will be moved to their own display library
 
-class Input:
+class Input (threading.Thread):
+	# This class extends threading.Thread so that each object can encapsulate
+	# its own thread.  I haven't overridden __init__() because I didn't see
+	# the need.  But we basically fall into the main loop when someone tells
+	# the object to start().  That loop then waits for characters over and
+	# over and over again until it recieves a keypress which tells it to stop
+	# OR userQuit gets set to True using the setHalt() method.
+
 	'A generic input class for the game engine to instantiate'
 	
 	inputChar = 0
-	inputEvents = queue.Queue()
+	inputEvents = Queue.Queue()
 	userQuit = False
 
-	def getInput(self, inputChar):
+	
 
+	# this is an EXTREMELY unsafe way to quit the
+	#     thread.  It doesn't lock in any way, but
+	#     it works for now as a demo.
+	def setHalt(self):
+		self.userQuit = True
+
+
+
+	# this method gets called when somebody tells this
+	#    object to start() ... all we do here is call
+	#    threadMainLoop() which runs until the thread
+	#    needs to die.  Then it's done!
+	def run(self):
+		print "THREAD %s STARTING!" % self.name
+
+		self.threadMainLoop()
+
+		print "THREAD %s STOPPING!  Goodbye!" % self.name
+
+
+
+
+
+
+	def threadMainLoop(self):
+		# this loop will run until self.userQuit becomes True
 		while self.userQuit is not True:
-	#			screenObject.stdscr.nodelay(1)
-	#			inputChar = screenObject.stdscr.getch()		#Get the input
-			if inputChar == 113:
-	#				dummyScreen.stopScreen()
-				self.userQuit = True
-			elif inputChar == 260:
-	#				dummyScreen.screenRefresh()			
-				self.storeInput(inputChar)
-			elif inputChar == 258:
-	#				dummyScreen.screenRefresh()			
-				self.storeInput(inputChar)
-			elif inputChar == 261:
-	#				dummyScreen.screenRefresh()
-				self.storeInput(inputChar)
-			else:
-				pass
+			# screenObject.stdscr.nodelay(1)
+
+			### get an input character
+			inputChar = 0
+			# inputChar = screenObject.stdscr.getch()		#Get the input
 			
-		return False
-				
+			### now handle the input
+			self.handleInput
+			
+			### for testing, sleep for 10 msec
+			time.sleep(0.01)
+			
+
+	# handleInput(inputChar) is called to handle an input
+	#    character.  This should only be called inside
+	#    threadMainLoop, but it can be called externally
+	#    for testing purposes.  Note that this is NOT
+	#    synchronized, so if it gets called in two places
+	#    at once, you'll have crazy race conditions. 
+	def handleInput(self, inputChar):
+		if inputChar == 113:
+			self.userQuit = True
+		elif inputChar == 260:
+			self.storeInput(inputChar)
+		elif inputChar == 258:
+			#				dummyScreen.screenRefresh()			
+			self.storeInput(inputChar)
+		elif inputChar == 261:
+			#				dummyScreen.screenRefresh()
+			self.storeInput(inputChar)
+		else:
+			pass
+
+	
 	def storeInput(self, keyPress):
-		Input.inputEvents.put(keyPress)
-		Input.inputEvents.task_done()
+		self.inputEvents.put(keyPress)
+		self.inputEvents.task_done()
 		return
 		
 	def dumpInput(self):
-		while not Input.inputEvents.empty():
-			return Input.inputEvents.get() #Just change this to return later for the game engine
+		while not self.inputEvents.empty():
+			return self.inputEvents.get() #Just change this to return later for the game engine
 
 
 #dummyInput = Input()
--- engine/unittests.py
+++ engine/unittests.py
@@ -1,29 +1,51 @@
 import unittest
+import time
 import inputManager
  
 class inputTests(unittest.TestCase):
 	
+	def testQuit(self):
+		newInput = inputManager.Input(name='testQuitInputManager')
+		newInput.start() # start the thread running
+		newInput.handleInput(113) # tell the thread to quit
+		time.sleep(0.1) # wait for the thread to die
+		self.assertEqual(False, newInput.is_alive(), 'Thread still alive after it was told to quit!')
+
+
+	def testHalt(self):
+		newInput = inputManager.Input(name='testHaltInputManager')
+		newInput.start() # start the thread running
+		
+		# manually kill the thread - note that this ISNT
+		# synchronized right now, so we need to do a
+		# longer wait after we send this signal:
+		newInput.setHalt() 
+		time.sleep(0.1)
+
+		self.assertEqual(False, newInput.is_alive(), 'Thread still alive after it was told to quit!')
+
+
+
 	def testLeft(self):
-		newInput = inputManager.Input()
-		newInput.getInput(260)
-		newInput.getInput(113)
+		newInput = inputManager.Input(name='testLeftInputManager')
+		newInput.start() # start the thread running
+		newInput.handleInput(260)
+		newInput.handleInput(113) # tell the thread to quit
 		self.assertEqual(newInput.dumpInput(), 260, 'I cannot go left')
 		
 	def testDown(self):
-		newInput = inputManager.Input()
-		newInput.getInput(258)
-		newInput.getInput(113)
+		newInput = inputManager.Input(name='testDownInputManager')
+		newInput.start() # start the thread running
+		newInput.handleInput(258)
+		newInput.handleInput(113) # tell the thread to quit
 		self.assertEqual(newInput.dumpInput(), 258, 'I cannot go down')
 		
 	def testRight(self):
-		newInput = inputManager.Input()
-		newInput.getInput(261)
-		newInput.getInput(113)
+		newInput = inputManager.Input(name='testRightInputManager')
+		newInput.start() # start the thread running
+		newInput.handleInput(261)
+		newInput.handleInput(113) # tell the thread to quit
 		self.assertEqual(newInput.dumpInput(), 261, 'I cannot go right')
-		
-	def testQuit(self):
-		newInput = inputManager.Input()
-		self.assertEqual(False, newInput.getInput(113), 'I cannot stop collecting input')
 	
 if __name__ == '__main__':
 	unittest.main()
