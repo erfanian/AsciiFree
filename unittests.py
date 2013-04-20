480e86be1033c88d8cf6f042f767eda577dd50de
Eric Erfanian <eric.erfanian@gmail.com>
2013-04-20 12:37:09 -0500

    Add no input catch.

--- unittests.py
+++ unittests.py
@@ -2,49 +2,60 @@ import unittest
 import time
 from engine import screenManager
 from engine import inputManager
- 
+
+
 class inputTests(unittest.TestCase):
-	
-	def testQuit(self):
-		newInput = inputManager.Input(name='testQuitInputManager')
-		newInput.start() # start the thread running
-		newInput.handleInput(113) # tell the thread to quit
-		time.sleep(0.1) # wait for the thread to die
-		self.assertEqual(False, newInput.is_alive(), 'Thread still alive after it was told to quit!')
+    def testQuit(self):
+        newInput = inputManager.Input(name='testQuitInputManager')
+        newInput.start() # start the thread running
+        newInput.handleInput(113) # tell the thread to quit
+        time.sleep(0.1) # wait for the thread to die
+        self.assertEqual(False, newInput.is_alive(), 'Thread still alive after it was told to quit!')
 
 
-	def testHalt(self):
-		newInput = inputManager.Input(name='testHaltInputManager')
-		newInput.start() # start the thread running
-		
-		# manually kill the thread - note that this ISNT
-		# synchronized right now, so we need to do a
-		# longer wait after we send this signal:
-		newInput.setHalt() 
-		time.sleep(0.1)
+    def testHalt(self):
+        newInput = inputManager.Input(name='testHaltInputManager')
+        newInput.start() # start the thread running
 
-		self.assertEqual(False, newInput.is_alive(), 'Thread still alive after it was told to quit!')
+        # manually kill the thread - note that this ISNT
+        # synchronized right now, so we need to do a
+        # longer wait after we send this signal:
+        newInput.setHalt()
+        time.sleep(0.1)
 
-	def testLeft(self):
-		newInput = inputManager.Input(name='testLeftInputManager')
-		newInput.start() # start the thread running
-		newInput.handleInput(260)
-		newInput.handleInput(113) # tell the thread to quit
-		self.assertEqual(newInput.dumpInput(), 260, 'I cannot go left')
-		
-	def testDown(self):
-		newInput = inputManager.Input(name='testDownInputManager')
-		newInput.start() # start the thread running
-		newInput.handleInput(258)
-		newInput.handleInput(113) # tell the thread to quit
-		self.assertEqual(newInput.dumpInput(), 258, 'I cannot go down')
-		
-	def testRight(self):
-		newInput = inputManager.Input(name='testRightInputManager')
-		newInput.start() # start the thread running
-		newInput.handleInput(261)
-		newInput.handleInput(113) # tell the thread to quit
-		self.assertEqual(newInput.dumpInput(), 261, 'I cannot go right')
-	
+        self.assertEqual(False, newInput.is_alive(), 'Thread still alive after it was told to quit!')
+
+
+def testLeft(self):
+    newInput = inputManager.Input(name='testLeftInputManager')
+    newInput.start() # start the thread running
+    newInput.handleInput(260)
+    newInput.handleInput(113) # tell the thread to quit
+    self.assertEqual(newInput.dumpInput(), 260, 'I cannot go left')
+
+
+def testDown(self):
+    newInput = inputManager.Input(name='testDownInputManager')
+    newInput.start() # start the thread running
+    newInput.handleInput(258)
+    newInput.handleInput(113) # tell the thread to quit
+    self.assertEqual(newInput.dumpInput(), 258, 'I cannot go down')
+
+
+def testRight(self):
+    newInput = inputManager.Input(name='testRightInputManager')
+    newInput.start() # start the thread running
+    newInput.handleInput(261)
+    newInput.handleInput(113) # tell the thread to quit
+    self.assertEqual(newInput.dumpInput(), 261, 'I cannot go right')
+
+
+def noInput(self):
+    newInput = inputManager.Input(name='testRightInputManager')
+    newInput.start() # start the thread running
+    newInput.handleInput(113) # tell the thread to quit
+    self.assertEqual(assertIsNone(newInput.dumpInput()), 'I recorded a value when I should not have.')
+
+
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
