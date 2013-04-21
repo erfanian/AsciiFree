5a248cf5c8a2d8016edf87d3465c1ef3105c3ce4
Eric Erfanian <eric.erfanian@gmail.com>
2013-04-21 18:45:54 -0500

    add license to inputManager

--- engine/inputManager.py
+++ engine/inputManager.py
@@ -3,15 +3,22 @@
 ## AsciiFree project #####################################################
 ## An open-source, ASCII-graphics version of SkiFree #####################
 ## Spring 2013 ###########################################################
+# This program is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
 ##########################################################################
-## This work is licensed under the #######################################
-## Attribution-ShareAlike 3.0 Unported (CC BY-SA 3.0) ####################
-## You can find the full license here: ###################################
-## http://creativecommons.org/licenses/by-sa/3.0/deed.en_US ##############
-##########################################################################
-## inputEngine.py contributors: ##########################################
-## Eric Erfanian #########################################################
-## Chris Cornelius #######################################################
+## filename.py contributors:                                             #
+## Eric Erfanian                                                         #
+## Chris Cornelius                                                       #
 ##########################################################################
 
 #http://docs.python.org/3.1/library/queue.html#module-queue
