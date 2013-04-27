08e8c2ad4359924729c276d11761ec6a3a05e4da
Eric Erfanian <eric.erfanian@gmail.com>
2013-04-27 12:38:04 -0500

    add license info

--- engine/DrawableObject.py
+++ engine/DrawableObject.py
@@ -1,8 +1,24 @@
+#! /usr/bin/env python3.0
 ##########################################################################
 ## AsciiFree project #####################################################
 ## An open-source, ASCII-graphics version of SkiFree #####################
 ## Spring 2013 ###########################################################
-## <<LICENSE INFO HERE>>
+# This program is free software: you can redistribute it and/or modify   #
+# it under the terms of the GNU General Public License as published by   #
+# the Free Software Foundation, either version 3 of the License, or      #
+# (at your option) any later version.                                    #
+#                                                                        #
+# This program is distributed in the hope that it will be useful,        #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of         #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the          #
+# GNU General Public License for more details.                           #
+#                                                                        #
+# You should have received a copy of the GNU General Public License      #
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  #
+##########################################################################
+## DrawableObject.py contributors:                                       #
+## Chris Cornelius                                                       #
+##########################################################################
 
 from SubclassMustImplementException import *
 
